5ca6f08951bf5ba7f515894e0c5814e4c3bf9106
athira-mechoor <athiramechoor@gmail.com>
2024-11-09 15:24:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+print("Trying to import StandardScaler from scikit-learn")
 from sklearn.preprocessing import StandardScaler
 
 # Load dataset
